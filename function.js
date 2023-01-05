456f8a6ee618af9709a56dc5cdc2890a48c0e13d
Isa_oliveira <belefreiresls@outloo.com>
2023-01-05 16:13:05 -0300

    Praticas de JavaScript

---
+++ function.js
@@ -0,0 +1,21 @@
+// constructor
+
+// function Person(name) {
+//     this.name = name;
+//     this.walk = function() {
+//         return this.name + " está andando";
+//     }
+// }
+
+// const isabele = new Person("Isabele");
+// const gabriel = new Person("Gabriel");
+// console.log(isabele.walk());
+// console.log(gabriel.walk());
+
+// let date = new Date("2022-12-01");
+
+// console.log(date);
+
+
+let name = new String("Isabele");
+console.log(name[0], name[1], name[2], name[3], name[4], name[5], name[6])
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+function Person(name) {
+//     this.name = name;
+//     this.walk = function() {
+//         return this.name + " está andando";
+//     }
+// }
+
+// const isabele = new Person("Isabele");
+// const gabriel = new Person("Gabriel");
+// console.log(isabele.walk());
+// console.log(gabriel.walk());
